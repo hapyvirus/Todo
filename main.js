7d8a7c1d3d81cce25b8d3d589ec58cb93356d795
hapyvirus <serenity2011@naver.com>
2025-02-11 15:31:13 +0900

    add Error Function

--- main.js
+++ main.js
@@ -8,41 +8,61 @@ app.use(express.json());
 
 await mongoose.connect(DATABASE_URL);
 
-app.post("/tasks", async (req, res) => {
-  const data = req.body;
-  console.log(data);
-  const newTask = await Task.create(data);
-  console.log(newTask);
-  res.status(201).send(newTask);
-});
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e.name === "CastError") {
+        res.status(404).send({ message: "Cannot find ID" });
+      } else if (e.name === "vaildationError") {
+        res.status(400).send({ mesasgs: e.message });
+      } else {
+        res.status(500).send({ mesage: e.message });
+      }
+    }
+  };
+}
 
-app.get("/tasks", async (req, res) => {
-  const count = Number(req.query.count) || 0;
-  const sortOption =
-    req.query.sort === "oldest" ? ["createdAt", "asc"] : ["createdAt", "desc"];
-  const tasks = await Task.find().limit(count).sort([sortOption]);
-  res.send(tasks);
-});
+app.post(
+  "/tasks",
+  asyncHandler(async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    const newTask = await Task.create(data);
+    console.log(newTask);
+    res.status(201).send(newTask);
+  })
+);
 
-app.get("/tasks/:id", async (req, res) => {
-  try {
+app.get(
+  "/tasks",
+  asyncHandler(async (req, res) => {
+    const count = Number(req.query.count) || 0;
+    const sortOption =
+      req.query.sort === "oldest"
+        ? ["createdAt", "asc"]
+        : ["createdAt", "desc"];
+    const tasks = await Task.find().limit(count).sort([sortOption]);
+    res.send(tasks);
+  })
+);
+
+app.get(
+  "/tasks/:id",
+  asyncHandler(async (req, res) => {
     const task = await Task.findById(req.params.id);
     if (task) {
       res.send(task);
     } else {
       res.status(404).send({ mesage: "Cannot find ID" });
     }
-  } catch (e) {
-    if (e.name === "CastError") {
-      res.status(404).send({ mesage: "Cannot find ID" });
-    } else {
-      res.status(500).send({ mesage: e.error });
-    }
-  }
-});
+  })
+);
 
-app.patch("/tasks/:id", async (req, res) => {
-  try {
+app.patch(
+  "/tasks/:id",
+  asyncHandler(async (req, res) => {
     const task = await Task.findById(req.params.id);
     if (task) {
       const data = req.body;
@@ -54,30 +74,19 @@ app.patch("/tasks/:id", async (req, res) => {
     } else {
       res.status(404).send({ mesage: "Cannot find ID" });
     }
-  } catch (e) {
-    if (e.name === "CastError") {
-      res.status(404).send({ mesage: "Cancnot find Id" });
-    } else {
-      res.status(500).send({ mesags: e.error });
-    }
-  }
-});
+  })
+);
 
-app.delete("/tasks/:id", async (req, res) => {
-  try {
+app.delete(
+  "/tasks/:id",
+  asyncHandler(async (req, res) => {
     const task = await Task.findByIdAndDelete(req.params.id);
     if (task) {
       res.sendStatus(200);
     } else {
       res.status(404).send({ mesage: "Cannot find ID " });
     }
-  } catch (e) {
-    if (e.name === "CastError") {
-      res.status(404).send({ mesage: "Cannot find ID" });
-    } else {
-      res.status(500).send({ mesage: e.error });
-    }
-  }
-});
+  })
+);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
